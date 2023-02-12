c3a5dc8a1bae9bd41d5abd4c36f701f0fbabe55a
ola13 <ola.piktus@gmail.com>
2023-02-12 19:42:04 +0000

    roots

--- web/em_server.py
+++ web/em_server.py
@@ -9,7 +9,7 @@ from socketserver import ThreadingMixIn
 
 import fasttext
 from bigscience_pii_detect_redact import run_pii
-from huggingface_hub import hf_hub_download
+from huggingface_hub import HfApi
 
 import argparse
 import pickle
@@ -22,8 +22,14 @@ from tqdm import tqdm
 
 
 MAX_DOCS = 5
-
-
+hf_api = HfApi()
+roots_datasets = {
+    dset.id.split("/")[-1]: dset
+    for dset in hf_api.list_datasets(
+        author="bigscience-data",
+        use_auth_token="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",
+    )
+}
 
 
 class ThreadedPyseriniHTTPServer(ThreadingMixIn, HTTPServer):
@@ -35,20 +41,25 @@ class ThreadedPyseriniHTTPServer(ThreadingMixIn, HTTPServer):
         logging.info("initializing suffix arrays")
         self.pos2id = pickle.load(
             open(
-                "/home/piktus_huggingface_co/lumi/dedup/oscar_025/oscar.train.pos2id.pkl", "rb"
+                "/home/piktus_huggingface_co/lumi/dedup/roots_en_10/roots_en.train.pos2id.pkl",
+                "rb",
             )
         )
         self.pos2id_list = sorted(self.pos2id.keys())
-        self.oscar = load_from_disk("/home/piktus_huggingface_co/lumi/preprocessed_data/oscar-dedup")
-
+        self.datasets = {}
+        for dataset in roots_datasets.keys():
+            if dataset.startswith("roots_en"):
+                self.datasets[dataset] = load_from_disk(
+                    "/home/piktus_huggingface_co/lumi/roots_en/" + dataset
+                )["train"]
 
     def get_doc_for_pos(self, pos):
         """
         Gets id of the datapoint at position.
         """
         pos = bisect_right(self.pos2id_list, pos)
-        doc_id = self.pos2id[self.pos2id_list[pos - 1]]
-        return self.oscar[doc_id]
+        dataset_name, doc_id = self.pos2id[self.pos2id_list[pos - 1]]
+        return dataset_name, doc_id, self.datasets[dataset_name][doc_id]
 
 
 class PyseriniHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -56,7 +67,6 @@ class PyseriniHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-
 
     def _process_result(self, doc, query):
         def find_whitespace(text):
@@ -75,16 +85,21 @@ class PyseriniHTTPRequestHandler(BaseHTTPRequestHandler):
         # print("start: {}, end: {}".format(start, end))
         return text[start:end]
 
-
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        logging.info(
+            "GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers)
+        )
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode("utf-8"))
 
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length).decode("utf-8")
-        logging.info("POST request,\nPath: {}\nHeaders:\n{}\nBody:\n{}\n".format(self.path, self.headers, post_data))
+        logging.info(
+            "POST request,\nPath: {}\nHeaders:\n{}\nBody:\n{}\n".format(
+                self.path, self.headers, post_data
+            )
+        )
 
         post_data = json.loads(post_data)
         if "flag" in post_data and bool(post_data["flag"]):
@@ -97,17 +112,20 @@ class PyseriniHTTPRequestHandler(BaseHTTPRequestHandler):
             exact_search = True
 
         query = post_data["query"]
-        k = MAX_DOCS if "k" not in post_data or post_data["k"] is None else int(post_data["k"])
+        k = (
+            MAX_DOCS
+            if "k" not in post_data or post_data["k"] is None
+            else int(post_data["k"])
+        )
         logging.info("Query: {}".format(query))
 
         query_bytes = query.encode("utf-8")
         tmp_file = "/tmp/fin_{}".format(uuid.uuid4())
         open(tmp_file, "wb").write(query_bytes)
 
-
         cmd = (
             "/home/piktus_huggingface_co/lumi/deduplicate-text-datasets/target/debug/dedup_dataset count-occurrences "
-            "--data-file  /home/piktus_huggingface_co/lumi/dedup/oscar_025/oscar.train "
+            "--data-file  /home/piktus_huggingface_co/lumi/dedup/roots_en_10/roots_en.train "
             "--query-file {}".format(tmp_file)
         )
 
@@ -115,14 +133,19 @@ class PyseriniHTTPRequestHandler(BaseHTTPRequestHandler):
         cmd_result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
         lines = cmd_result.stdout.decode("utf-8").split("\n")
 
-
         prefix = "Found at: "
         results = []
         for line in tqdm(lines):
             if line.startswith(prefix):
                 pos = int(line.strip()[len(prefix) :])
-                doc = self.server.get_doc_for_pos(pos)
-                results.append(self._process_result(doc, query))
+                dataset_name, doc_id, doc = self.server.get_doc_for_pos(pos)
+                results.append(
+                    {
+                        "text": self._process_result(doc, query),
+                        "ds": dataset_name,
+                        "id": doc_id,
+                    }
+                )
 
         payload = {"results": results}
         self._set_response()
@@ -131,7 +154,9 @@ class PyseriniHTTPRequestHandler(BaseHTTPRequestHandler):
 
 def run(index_dir, server_address, port):
     logging.basicConfig(level=logging.INFO)
-    httpd = ThreadedPyseriniHTTPServer((server_address, port), PyseriniHTTPRequestHandler, index_dir)
+    httpd = ThreadedPyseriniHTTPServer(
+        (server_address, port), PyseriniHTTPRequestHandler, index_dir
+    )
 
     sa = httpd.socket.getsockname()
     logging.info("Starting httpd on {} port {} ...".format(sa[0], sa[1]))
@@ -160,7 +185,11 @@ if __name__ == "__main__":
         help="Address of the server, e.g. '12.345.678.910'",
     )
     parser.add_argument(
-        "-p", "--port", type=int, default=8080, help="Port on which to serve ",
+        "-p",
+        "--port",
+        type=int,
+        default=8080,
+        help="Port on which to serve ",
     )
     args = parser.parse_args()
     run(args.index_dir, args.server_address, args.port)
